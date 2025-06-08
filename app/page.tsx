f8261742c6bdfa7265f72afa5072085717b4f77c
Bastbi12 <bbrim@gmx.de>
2025-06-08 22:52:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import Dashboard from '@/modules/dashboard/Dashboard';
-export default function Home() {
-  return <Dashboard />;
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-4">Dashboard widgets placeholder</h1>
+      {/* Hier kommen später deine Widgets rein */}
+    </>
+  );
 }
